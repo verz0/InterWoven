d792fcd3895caad010b763422393b522c52c1531
Pirajesh <pirajeshmr@gmail.com>
2023-12-08 21:22:44 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import './globals.css'
+import AuthLayout from './_auth/AuthLayout';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
+import {Home} from './_root/pages';
+import './globals.css';
 
+import { Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
-    <div>App</div>
+    <main className='flex h-screen'>
+    <Routes>
+      <Route element = {<AuthLayout/>}>
+      <Route path='/sign-in' element= {<SigninForm/>}/>
+      <Route path='/sign-up' element= {<SignupForm/>}/>
+      </Route>
+
+      <Route element = {<RootLayout/>}>
+      <Route index element={<Home/>}/>
+      </Route>
+    </Routes>
+    </main>
   )
 }
 
