fcd321129937244b01bb1d36270540f15982ad77
Pirajesh <pirajeshmr@gmail.com>
2023-12-28 00:05:29 +0530

    added account creation function

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,6 +15,7 @@ import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import { Loader } from "lucide-react";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -28,8 +29,10 @@ const SignupForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    //const newUser = await createUserAccount(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await createUserAccount(values);
+
+    console.log(newUser)
   }
 
   return (
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,20 @@
-export async function createUserAccount(user){
+import { INewUser } from "@/types";
+import { ID } from "appwrite";
+import { account } from "./config";
+export async function createUserAccount(user:INewUser){
+
+    try{
+        const newAccount = await account.create(
+         ID.unique(),
+         user.email,
+         user.name,
+         user.password,
     
+        )
+       return newAccount;
+    } catch (error){
+        console.log(error);
+        return error;
+    }
+     
 }
