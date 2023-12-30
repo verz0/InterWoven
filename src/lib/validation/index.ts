dc923ada9d2eca8ea739feceb900cda062a91a53
Pirajesh <pirajeshmr@gmail.com>
2023-12-30 09:15:45 +0530

    minor changes

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,8 @@
 import * as z from "zod"
 
 export const SignupValidation = z.object({
-    name :z.string().min(2, {message : 'too short'}),
-    username: z.string().min(2, {message : 'too short'}),
+    name :z.string().min(2, {message : 'Too short!'}),
+    username: z.string().min(2, {message : 'User should have at least 5 characters!'}),
     email: z.string().email(),
-    password :z.string().min(8, {message : 'too short'}),
+    password :z.string().min(8, {message : 'Password should have at least 8 characters!'}),
   });
